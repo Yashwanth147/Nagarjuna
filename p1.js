5f9356dfaf1ced30225b0c3f7b16d555228760d1
Yashwanth147 <yashwanth0549@gmail.com>
2025-06-17 14:15:03 +0530

    Update p1.js
    commited in line 6

--- p1.js
+++ p1.js
@@ -3,3 +3,4 @@ let num1=parseFloat(prompt("Enter the first number: "));
 let num2=parseFloat(prompt("Enter the second number: "));
 let sum = num1+num2;
 alert("The sum of " +num1+ " and " + num2 + " is "+ sum);
+conole.log("hello world!");
